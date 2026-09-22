7a4e593daa3ee77c3113029d47fcec9285c56e16
EnzoHashinokutiXavier <enzokuti.xavier@gmail.com>
2026-09-22 15:59:23 -0400

    feat: implement basic Flask app with Jinja template

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,1 +1,15 @@
-# arquivo principal : main.py ou app.py
+from pathlib import Path
+from flask import Flask, render_template
+
+BASE_DIR = Path(__file__).resolve().parent.parent.parent  # backend/app -> backend -> raiz do projeto
+TEMPLATES_DIR = BASE_DIR / "frontend" / "templates"
+STATIC_DIR = BASE_DIR / "frontend" / "static"
+
+app = Flask(__name__, template_folder=str(TEMPLATES_DIR), static_folder=str(STATIC_DIR))
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run()
